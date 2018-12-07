cedfad218619e13bd16e3371a121147025bf9f15
a1ex4 <12754559+a1ex4@users.noreply.github.com>
2018-12-07 19:54:48 +0100

    Moved all the shell commands in the Android class

--- __init__.py
+++ __init__.py
@@ -13,11 +13,9 @@ class ADBClient:
 
     def server_is_connected(self):
         """ Check if we can connect to the ADB server. """
-
         try:
             self._adb_devices = self._client.devices()
             return True
-
         except RuntimeError:
             return False
 
@@ -38,7 +36,6 @@ class Android:
     def __init__(self, host, client, adb_device):
         """ Initialize AndroidTV object.
         :param host: Host in format <address>:port.
-        :param apps: Custom user known apps
         :param client: ADB client object
         :param adb_device: ADB device object
         """
@@ -56,8 +53,8 @@ class Android:
 
     def update(self):
         # State detection
-        power_output = self.shell('dumpsys power')
-        audio_output = self.shell('dumpsys audio')
+        power_output = self._adb_shell('dumpsys power')
+        audio_output = self._adb_shell('dumpsys audio')
 
         if 'Display Power: state=ON' not in power_output:
             self._state = 'off'
@@ -90,13 +87,75 @@ class Android:
 
 
     def current_app(self):
-        win_output = self.shell('dumpsys window windows')
+        win_output = self._adb_shell('dumpsys window windows')
         for line in win_output.splitlines():
             if 'mCurrentFocus' in line:
                 current_app = line.split(' ')[4].split('/')[0]
                 return current_app
         return None
 
-    def shell(self, command):
+    def _adb_shell(self, command):
         """ Execute a shell command on the device"""
-        return(self. _adb_device.shell(command))
+        return(self._adb_device.shell(command))
+
+    def turn_on(self):
+        """Instruct the tv to turn on."""
+        self._adb_shell('input keyevent 26')
+
+    def turn_off(self):
+        """Instruct the tv to turn off."""
+        self._adb_shell('input keyevent 26')
+
+    def media_play(self):
+        """Send play command."""
+        self._adb_shell('input keyevent 126')
+        self._state = STATE_PLAYING
+
+    def media_pause(self):
+        """Send pause command."""
+        self._adb_shell('input keyevent 127')
+        self._state = STATE_PAUSED
+
+    def media_play_pause(self):
+        """Send play/pause command."""
+        self._adb_shell('input keyevent 85')
+
+    def media_stop(self):
+        """Send stop command."""
+        self._adb_shell('input keyevent 86')
+        self._state = STATE_IDLE
+
+    def mute_volume(self, mute):
+        """Mute the volume."""
+        self._adb_shell('input keyevent 164')
+        self._muted = mute
+
+    def volume_up(self):
+        """Increment the volume level."""
+        self._adb_shell('input keyevent 24')
+
+    def volume_down(self):
+        """Decrement the volume level."""
+        self._adb_shell('input keyevent 25')
+
+    def media_previous_track(self):
+        """Send previous track command."""
+        self._adb_shell('input keyevent 88')
+
+    def media_next_track(self):
+        """Send next track command."""
+        self._adb_shell('input keyevent 87')
+
+    def input_key(self, key):
+        """Input the key to the device."""
+        self._adb_shell("input keyevent {}".format(key))
+
+    def start_intent(self, uri):
+        """Start an intent on the device."""
+        self._adb_shell(
+            "am start -a android.intent.action.VIEW -d {}".format(uri))
+
+    def do_action(self, action):
+        """Input the key corresponding to the action."""
+        self._adb_shell(
+            "input keyevent {}".format(ACTIONS[action]))
